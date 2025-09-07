161ac9d9f63e4a57ab780404849b4f5cdda871c0
Franklin <franklineleazar20gmail.com>
2025-09-07 13:26:02 +0200

    routa de prueba en users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// ruta get de prueba 
+router.get('/cool', function(req, res, next){
+  res.render('cool')
+})
+
 module.exports = router;
+
