bc6a84e4e4d04e3c242bd683158ff43c804872ef
Franklin <franklineleazar20gmail.com>
2025-09-07 20:27:29 +0200

    Conectar a mongoDB atlas e inyectar datos de prueba

---
+++ models/genre.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100,
+  },
+});
+
+// Virtual para la URL del género
+GenreSchema.virtual('url').get(function () {
+  return `/catalog/genre/${this._id}`;
+});
+
+// Exportar el modelo
+module.exports = mongoose.model('Genre', GenreSchema);
